0f019555f3a32d9b4f5609d73af9adac5db008b1
sofvckinmadguy <segalzakhar1999@mail.ru>
2020-07-09 03:02:44 +0300

    added Profile and FindFriendsScreen

---
+++ Profile/Profile.js
@@ -0,0 +1,13 @@
+// function scroll() {
+//     var lastScrollTop = 0;
+//     navBar = document.getElementById("navbar");
+//     window.addEventListener("scroll", function () {
+//         var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//         if (scrollTop > lastScrollTop) {
+//             navBar.style.top = "-80px";
+//         } else {
+//             navBar.style.top = "0";
+//         }
+//         lastScrollTop = scrollTop;
+//     })
+// }
